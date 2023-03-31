79f325f0d0bde7f5f78318e855b826682c480f15
Abner Kaizer <abnerkaizer@protonmail.com>
2023-03-31 09:34:39 -0300

    layout, pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
+import {Route,Switch,Link,BrowserRouter} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
 function App() {
+
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+
+    <BrowserRouter>
+
+      <div>
+
+        <Link to='/'>Home</Link>
+
+        <Link to='/contact'>Contato</Link>
+
+        <Link to='/company'>Empresa</Link>
+
+        <Link to='/newproject'>Novo Projeto</Link>
+
+      </div>
+
+      <Container customClass='min-height'>
+
+        <Switch>
+
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+
+          <Route exact path='/company'>
+            <Company/>
+          </Route>
+
+          <Route exact path='/contact'>
+            <Contact/>
+          </Route>
+
+          <Route exact path='/newproject'>
+            <NewProject/>
+          </Route>
+
+        </Switch>
+
+      </Container>
+
+      <p>Footer</p>
+
+    </BrowserRouter>
+
   );
+
 }
 
 export default App;
