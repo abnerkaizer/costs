3f0abaaf16d7ab6db742e51eb63714f2b0137fbe
Abner Kaizer <abnerkaizer@protonmail.com>
2023-03-31 10:17:26 -0300

    navbar, footer

--- src/App.js
+++ src/App.js
@@ -1,57 +1,39 @@
-import {Route,Switch,Link,BrowserRouter} from "react-router-dom";
+import {Route,Switch,BrowserRouter} from "react-router-dom";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
+import Projects from "./components/pages/Projects";
 import NewProject from "./components/pages/NewProject";
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
 function App() {
-
   return (
-
     <BrowserRouter>
-
-      <div>
-
-        <Link to='/'>Home</Link>
-
-        <Link to='/contact'>Contato</Link>
-
-        <Link to='/company'>Empresa</Link>
-
-        <Link to='/newproject'>Novo Projeto</Link>
-
-      </div>
-
+      <Navbar/>
       <Container customClass='min-height'>
-
         <Switch>
-
           <Route exact path='/'>
             <Home/>
           </Route>
-
           <Route exact path='/company'>
             <Company/>
           </Route>
-
+          <Route exact path='/projects'>
+            <Projects/>
+          </Route>
           <Route exact path='/contact'>
             <Contact/>
           </Route>
-
           <Route exact path='/newproject'>
             <NewProject/>
           </Route>
-
         </Switch>
-
       </Container>
-
-      <p>Footer</p>
-
+      <Footer/>
     </BrowserRouter>
-
   );
-
 }
 
 export default App;
