9874ccba3e5380eb6279413f473dcd2e5dac993c
Abner Kaizer <abnerkaizer@protonmail.com>
2023-04-01 20:52:51 -0300

    projects layout

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,13 +3,32 @@ import Message from "../layout/Message"
 import styles from './Projects.module.css'
 import Container from "../layout/Container"
 import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard"
+import { useEffect, useState } from "react"
 
 function Projects() {
+    const [projects, setProjects] = useState([])
+
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/projects",{//request à api
+        method: "GET",//metodo get
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        .then((resp)=> resp.json())//pegou a resposta transformou em json
+        .then((data)=>{//pega os dados e armazena no hook
+            setProjects(data)
+        })
+        .catch((err)=>console.log(err))
+    },[])
+
     return(
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -18,7 +37,17 @@ function Projects() {
             </div>
             {message && (<Message type="success"message={message} time={3000}/>)}
             <Container customClass="start">
-                <p>Projetos ...</p>
+                {projects.length > 0 && 
+                    projects.map((project)=>(
+                        <ProjectCard 
+                            id = {project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project?.category?.name}
+                            key={project.id}
+                        />
+                    ))
+                }
             </Container>
         </div>
     )
