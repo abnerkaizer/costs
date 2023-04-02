d49ebedc9170612221bda3c5922c81bdae169928
Abner Kaizer <abnerkaizer@protonmail.com>
2023-04-02 16:24:06 -0300

    services

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -11,6 +11,7 @@ function Project() {
     const [project,setProject] = useState([])
     const [removeLoading,setRemoveLoading] = useState(false)
     const [showProjectForm, setShowProjectForm] = useState()
+    const [showServiceForm, setShowServiceForm] = useState()
     const [message,setMessage] = useState()
     const [type,setType] = useState()
 
@@ -33,7 +34,11 @@ function Project() {
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
     }
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
+    }
     function editPost(project) {
+        setMessage('')
         //budget validation
         if (project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto.')
@@ -86,6 +91,23 @@ function Project() {
                                 </div>
                             )}
                         </div>
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.button} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar' : 'Fechar'}
+                            </button>
+                            <div className={styles.project_info} >
+                                {showServiceForm && (
+                                    <div>Formulário do serviço</div>
+                                )
+
+                                }
+                            </div>
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass="start">
+                            <p>Itens de serviços</p>
+                        </Container>
                     </Container>
                 </div>
             ) : (
